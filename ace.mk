3b388e74a37a2c4f64afb54cb0fed2a74a0b1030
Justin Schwartz <schwjm@users.noreply.github.com>
2010-04-22 21:36:48 -0500

    devised a method to let ace get the full EXTRA_INCLUDE_DIRS paths by making sure the acehandlers are called after all other .mk files are processed. requires the callbacks makefile hack, which may or may not be part of an official asss release. http://bitbucket.org/grelminar/asss/changeset/cd76b79f5c00 fixes issue 6

--- ace.mk
+++ ace.mk
@@ -1,6 +1,13 @@
 
 ACE_SOURCES=$(wildcard */*.aces)
 
+define regacecallback
+
+CALLBACKS := $(CALLBACKS) acehandler,$(1),$(2)
+
+endef
+
+
 define acehandler
 
 -include $(subst .acec,.d,$1)
@@ -12,7 +19,7 @@ $(1): $(2)
 $(1:.acec=.o): $(1) $(2)
 	$(CC) $(CFLAGS) -I$(dir $(2)) -c -x c -o $(1:.acec=.o) $(1)
 	$(CC) $(CFLAGS) -I$(dir $(2)) -c -x c -MF $(1:.acec=.d) -MT $(1:.acec=.o) -MM $(1)
+
 endef
 
-$(foreach src,$(ACE_SOURCES),$(eval $(call acehandler,$(addprefix ../build/,$(subst .aces,.acec,$(notdir $(src)))),$(src))))
-
+$(foreach src,$(ACE_SOURCES),$(eval $(call regacecallback,$(addprefix ../build/,$(subst .aces,.acec,$(notdir $(src)))),$(src))))
