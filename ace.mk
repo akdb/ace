14377bd4c1fc37c7592061cf5e4a3e3efd0654db
Justin Schwartz <schwjm@users.noreply.github.com>
2010-04-22 19:11:24 -0500

    ace.mk adds -Isrcfolder/ to the cc command line, so #include "fileinsamefolder.h" will work from src/srcfolder/file.aces fixes issue 15

--- ace.mk
+++ ace.mk
@@ -10,8 +10,8 @@ $(1): $(2)
 	python ace/ace.py -l -o $(1) $(2)
 
 $(1:.acec=.o): $(1) $(2)
-	$(CC) $(CFLAGS) -c -x c -o $(1:.acec=.o) $(1)
-	$(CC) $(CFLAGS) -c -x c -MF $(1:.acec=.d) -MT $(1:.acec=.o) -MM $(1)
+	$(CC) $(CFLAGS) -I$(dir $(2)) -c -x c -o $(1:.acec=.o) $(1)
+	$(CC) $(CFLAGS) -I$(dir $(2)) -c -x c -MF $(1:.acec=.d) -MT $(1:.acec=.o) -MM $(1)
 endef
 
 $(foreach src,$(ACE_SOURCES),$(eval $(call acehandler,$(addprefix ../build/,$(subst .aces,.acec,$(notdir $(src)))),$(src))))
